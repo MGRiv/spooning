06272a791bb614f8156f161ed2d3d816b966fa45
MGRiv <mgrivera501@gmail.com>
2016-05-02 00:47:59 -0400

    map & filter

--- svg.js
+++ svg.js
@@ -1,6 +1,11 @@
 var canvas = document.getElementById("canvas");
+var a = document.getElementById("a");
 var b = document.getElementById("b");
+var c = document.getElementById("c");
+var d = document.getElementById("d");
+var e = document.getElementById("e");
 var l = [];
+var ccheck = -1;
 
 var randColor = function randColor(e){
     var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
@@ -35,6 +40,18 @@ var makeBall = function makeBall(e){
     var y = function(){
         return posy;
     }
+    var gxdiff = function (){
+	return xdiff;
+    }
+    var gydiff = function (){
+	return ydiff;
+    } 
+    var cxdiff = function (input){
+        xdiff = input;
+    }
+    var cydiff = function (input){
+        ydiff = input;
+    }
     var changepos = function(){
 	console.log("blah");
 	posx += xdiff;
@@ -49,22 +66,28 @@ var makeBall = function makeBall(e){
 	c2.setAttribute("cy", posy);
     };
     var collide = function(){
-	var c;
-	for(c = 0; c < l.length; c++){
-	    var temp = l[c];
-	    if(temp.x() != posx && temp.y() != posy && temp.xdiff != xdiff && temp.ydiff != ydiff && temp.r != r){
-		if(Math.sqrt((posx - temp.x()) * (posx - temp.x()) + (posy - temp.y()) * (posy - temp.y())) < r + temp.r){
-		    xdiff *= -1;
-		    ydiff *= -1;
-		    temp.xdiff *= -1;
-		    temp.ydiff *= -1;
-		    changepos();
-		    temp.changepos();
+	if(ccheck == 1){
+	    var c;
+	    for(c = 0; c < l.length; c++){
+		var temp = l[c];
+		if(temp.x() != posx && temp.y() != posy && temp.xdiff != xdiff && temp.ydiff != ydiff && temp.r != r){
+		    if(Math.sqrt((posx - temp.x()) * (posx - temp.x()) + (posy - temp.y()) * (posy - temp.y())) < r + temp.r){
+			xdiff *= -1;
+			ydiff *= -1;
+			temp.xdiff *= -1;
+			temp.ydiff *= -1;
+			changepos();
+			temp.changepos();
+		    }
 		}
 	    }
 	}
     }
     return {
+	cxdiff : cxdiff,
+	cydiff : cydiff,
+	gxdiff : gxdiff,
+	gydiff : gydiff,
 	changepos : changepos,
 	collide : collide,
 	x : x,
@@ -82,4 +105,39 @@ var add = function add(e) {
     setInterval(newball.collide,16);
 };
 
+var ccheckcheck = function ccheckcheck(e){
+    ccheck *= -1;
+};
+
+var dup = function dup(e){
+    l.map(function(x) {
+	x.cxdiff(x.gxdiff() * 1.4);
+	x.cydiff(x.gydiff() * 1.4);
+	return x;
+    });
+};
+
+var speeding = function speeding(e){
+    var caught = [];
+    caught = l.filter(function(x){
+	return Math.sqrt(Math.pow(x.gxdiff(),2) + Math.pow(x.gydiff(),2)) >= 20;
+    });
+    var speedcount;
+    for(speedcount = 0; speedcount < caught.length; speedcount++){
+	caught[speedcount].cxdiff(1);
+	caught[speedcount].cydiff(1);
+    }
+};
+
+var bye = function bye(e){
+    if(canvas.childElementCount != 0){
+	canvas.removeChild(canvas.children[canvas.childElementCount - 1]);
+	l.pop();
+    }
+};
+
+e.addEventListener("click",bye);
+d.addEventListener("click",dup);
+c.addEventListener("click",ccheckcheck);
 b.addEventListener("click",add);
+a.addEventListener("click",speeding);
